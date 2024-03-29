5e0b12b7c6846fbe44d77519c75315ce20edf9d3
Leonardo Bonifacio Neves <lbonifacioneves@gmail.com>
2024-03-29 19:05:04 -0300

    Initial commit

---
+++ src/main/java/br/com/controle/user/controller/UserControllerApplication.java
@@ -0,0 +1,13 @@
+package br.com.controle.user.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserControllerApplication.class, args);
+	}
+
+}
